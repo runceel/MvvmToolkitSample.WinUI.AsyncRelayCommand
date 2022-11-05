c3cf307db0c60233c99adef5eea656497cd53ddd
Kazuki Ota <k_ota28@hotmail.com>
2022-11-05 20:25:01 +0900

    プロジェクト ファイルを追加します。

---
+++ MvvmToolkitSample.WinUI/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmToolkitSample.WinUI;
+
+public partial class MainViewModel : ObservableObject
+{
+    [RelayCommand]
+    private async Task<string> SayHello()
+    {
+        await Task.Delay(3000);
+        return $"Hello world at {DateTime.Now.ToString(CultureInfo.InvariantCulture)}";
+    }
+}
---
+++ MvvmToolkitSample.WinUI/TaskResultConverter.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Common;
+using Microsoft.UI.Xaml.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace MvvmToolkitSample.WinUI;
+
+public sealed class TaskResultConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is Task task)
+        {
+            return task.GetResultOrDefault();
+        }
+        else if (value is null)
+        {
+            return null;
+        }
+
+        // Otherwise, we'll just pass through whatever value/result was given to us.
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotSupportedException();
+    }
+}
